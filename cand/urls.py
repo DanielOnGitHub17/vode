52d5f7c6f33dff4329ab3eae33d139541ee865ab
Daniel Enesi <enesidaniel.120064@gmail.com>
2025-10-18 15:47:25 -0500

    Refactor dashboard view to action view and update URL routing; add signin app structure and templates

--- cand/urls.py
+++ cand/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import DashboardView
+from .views import ActionView
 
 app_name = "cand"
 
 urlpatterns = [
-    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("action/", ActionView.as_view(), name="action"),
 ]
--- cand/views.py
+++ cand/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
+from datetime import datetime, timedelta
 
 # Create your views here.
 
-class DashboardView(View):
+class ActionView(View):
     def get(self, request, *args, **kwargs):
         # Handle GET request
-        context = {}
-        return render(request, 'dashboard.html', context)
+        # TODO: Fetch from database later
+        context = {
+            'role': {
+                'title': 'Senior Software Engineer',
+                'description': 'Join our team as a Senior Software Engineer working on cutting-edge cloud infrastructure and distributed systems. You will be responsible for designing and implementing scalable solutions that power millions of users worldwide.',
+            },
+            'round': {
+                'number': 1,
+                'start_time': (datetime.now() + timedelta(hours=2)).strftime('%I:%M %p, %B %d, %Y'),
+            }
+        }
+        return render(request, "cand/index.html", context)
     
     def post(self, request, *args, **kwargs):
         # Handle POST request
