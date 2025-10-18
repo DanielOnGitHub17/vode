e27831b78f56117e59a21e1026e8f6a4731441ff
Daniel Enesi <enesidaniel.120064@gmail.com>
2025-10-18 15:32:50 -0500

    Add candidate app URLs and dashboard view implementation

--- cand/views.py
+++ cand/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
 # Create your views here.
+
+class DashboardView(View):
+    def get(self, request, *args, **kwargs):
+        # Handle GET request
+        context = {}
+        return render(request, 'dashboard.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        # Handle POST request
+        return JsonResponse({'status': 'success'})
